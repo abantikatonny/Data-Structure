c31cb44e194453a09dc11985bd0da3360e8cfc3c
Abantika Das Tonny <45185113+abantikatonny@users.noreply.github.com>
2021-10-07 20:04:52 -0700

    Update Linked_list.py

--- Linked_list.py
+++ Linked_list.py
@@ -1,26 +1,19 @@
+from random import randint
+
+
 class Node:
-    def __init__(self, a_value = None, b_value = None):
+    def __init__(self, a_value=None, b_value=None):
         self.a = a_value
         self.b = b_value
-
 
         self.link = None
 
     def print_values(self):
         print(self.a, self.b)
 
-
     def count_from_node(self):
         x = 0
         y = self
-
-
-        for i in range(5):
-            y.link = Node(i, i*3)
-            y = y.link
-
-            y.print_values()
-            
 
         counter = 0
         while True:
@@ -28,36 +21,61 @@ class Node:
             y.print_values()
             y = y.link
 
-            if y.link != None:
-                y = y.link
+            if y == None:
+                break
 
-            print(counter)
-
-
-
-head = Node(5,6)
-# head.print_values()
-# print(head.a, head.b)
-head.b = head.a + head.b
-# print(head.a, head.b)
-
-b = Node(56, 12)
-# b.print_values()
-
-head.link = b
-# head.link.print_values()
-
-c = Node(9, 0)
-b.link = c
-# b.link.print_values()
+        print(counter)
+        return counter
 
 
-d = Node(2, 3)
-d.link = head
-# d.print_values()
-# d.link.print_values()
+def auto_generate_nodes(L):
+    """
+    Given the expected length L, this function will create a linked list of length L and return the head.
+
+    Parameters:
+    ----------
+    L: the expected length of linked list.
+
+    Return:
+    -------
+    The head of a linked list of length L.
+
+    """
+
+    head = Node(6, 8)
+    last = head
+
+    for i in range(L-1):
+        last.link = Node(i+1, 8)
+        last = last.link
+
+    return head
 
 
+
+# head = Node(5, 6)
+# # head.print_values()
+# # print(head.a, head.b)
+# head.b = head.a + head.b
+# # print(head.a, head.b)
+#
+# b = Node(56, 12)
+# # b.print_values()
+#
+# head.link = b
+# # head.link.print_values()
+#
+# c = Node(9, 0)
+# b.link = c
+# # b.link.print_values()
+#
+#
+# d = Node(2, 3)
+# d.link = head
+# # d.print_values()
+# # d.link.print_values()
+# # d.count_from_node()
+# d.auto_generate_nodes()
 
 # current = d
 # for i in range(5):
@@ -80,15 +98,6 @@ d.link = head
 # print(counter)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+x = auto_generate_nodes(10)
+x.print_values()
+x.count_from_node()
