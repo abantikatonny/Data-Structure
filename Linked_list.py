9ee387c3eb998a6b7d250760b76f0dd119445fd8
Abantika Das Tonny <45185113+abantikatonny@users.noreply.github.com>
2022-02-10 20:56:15 -0800

    Update Linked_list.py

--- Linked_list.py
+++ Linked_list.py
@@ -1,103 +1,36 @@
-from random import randint
-
-
 class Node:
-    def __init__(self, a_value=None, b_value=None):
-        self.a = a_value
-        self.b = b_value
+    def __init__(self, data = None, next= None):
+        self.data = data
+        self.next = next
 
-        self.link = None
+class LinkedList:
+    def __init__(self):
+        self.head = None
 
-    def print_values(self):
-        print(self.a, self.b)
+    def insert_at_begining(self, data):
+        node = Node(data, self.head)
+        self.head = node
 
-    def count_from_node(self):
-        x = 0
-        y = self
+    def print(self):
+        if self.head is None:
+            print("empty")
+            return
+        itr = self.head
+        nwstr = ""
 
-        counter = 0
-        while True:
-            counter += 1
-            y.print_values()
-            y = y.link
+        while itr:
+            nwstr += str(itr.data) + "---->"
+            itr = itr.next
+        print(nwstr)
 
-            if y == None:
-                break
-
-        print(counter)
-        return counter
-
-
-def auto_generate_nodes(L):
-    """
-    Given the expected length L, this function will create a linked list of length L and return the head.
-
-    Parameters:
-    ----------
-    L: the expected length of linked list.
-
-    Return:
-    -------
-    The head of a linked list of length L.
-
-    """
-
-    head = Node(6, 8)
-    last = head
-
-    for i in range(L-1):
-        last.link = Node(i+1, 8)
-        last = last.link
-
-    return head
+if __name__ == "__main__":
+    ll = LinkedList()
+    ll.insert_at_begining(5)
+    ll.insert_at_begining(10)
+    ll.print()
 
 
 
-# head = Node(5, 6)
-# # head.print_values()
-# # print(head.a, head.b)
-# head.b = head.a + head.b
-# # print(head.a, head.b)
-#
-# b = Node(56, 12)
-# # b.print_values()
-#
-# head.link = b
-# # head.link.print_values()
-#
-# c = Node(9, 0)
-# b.link = c
-# # b.link.print_values()
-#
-#
-# d = Node(2, 3)
-# d.link = head
-# # d.print_values()
-# # d.link.print_values()
-# # d.count_from_node()
-# d.auto_generate_nodes()
-
-# current = d
-# for i in range(5):
-#     current.link = Node(i, i*3)
-#     current = current.link
-#
-#     current.print_values()
 
 
-# current = d
-# counter = 0
-# while True:
-#     counter += 1
-#     current.print_values()
-#     current = current.link
-#
-#     if current == None:
-#         break
-#
-# print(counter)
 
-
-x = auto_generate_nodes(10)
-x.print_values()
-x.count_from_node()
